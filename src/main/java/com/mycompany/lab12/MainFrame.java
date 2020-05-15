0a470e009351ae846569366bd093522e1e156958
Palalae Stefan <palalaestefan301@gmail.com>
2020-05-15 17:21:37 +0300

    1st commit

---
+++ src/main/java/com/mycompany/lab12/MainFrame.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.lab12;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Palalae
+ */
+public class MainFrame extends JFrame{
+    JPanel ControlPanel;
+    JPanel  DesignPanel;
+}
